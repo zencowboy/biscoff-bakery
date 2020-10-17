b94691c10792f23585a008b30f39cd6f065561fa
mitchelldeyoung <mitchelldeyoung@Mitchs-MacBook-Pro.local>
2020-10-17 14:52:40 +0800

    first commit

---
+++ models/bakedgoods.js
@@ -0,0 +1,10 @@
+const bakedGoods = [
+  { name: 'Biscoff Spready Gooey Butter Cake', slug: 'biscoff-spready-gooey-butter-cake', price: 25, image: 'https://img.huffingtonpost.com/asset/5b9c6b3e2500003300360ed3.png' },
+  { name: 'Biscoff Brownie Cake', slug: 'biscoff-brownie-cake', price: 35, image: 'https://img.huffingtonpost.com/asset/5b9c6b3f2200005600d93aba.jpeg' },
+  { name: 'Biscoff and Kahlua Crunch Cupcakes', slug: 'biscoff-and-kahlua-crunch-cupcakes', price: 15, image: 'https://img.huffingtonpost.com/asset/5b9c6b402000003000fd8fc7.jpeg' },
+  { name: 'Chocolate Biscoff Whoopie Pies', slug: 'chocolate-biscoff-whoopie-pies', price: 10, image: 'https://img.huffingtonpost.com/asset/5b9c6b401f000050002135eb.jpeg' },
+  { name: 'Biscoff Banana Muffins', price: 5, slug: 'biscoff-banana-muffins', image: 'https://img.huffingtonpost.com/asset/5b9c6b42240000300094aad3.jpeg' },
+  { name: 'Biscoff Pumpkin Cheesecake', slug: 'biscoff-pumpkin-cheesecake', price: 20, image: 'https://img.huffingtonpost.com/asset/5b9c6b431f000050002135f0.jpeg' },
+]
+
+module.exports = bakedGoods;
---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log("Biscoff app ready")
